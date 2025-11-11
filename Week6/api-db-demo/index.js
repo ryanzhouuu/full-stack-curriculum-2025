3ccab42cac76e8646b6f28a6ea418f20e44e718a
Ryan Zhou <ryanzhouuu@gmail.com>
2025-11-10 22:21:32 -0600

    api db demo

--- Week6/api-db-demo/index.js
+++ Week6/api-db-demo/index.js
@@ -1,3 +1,28 @@
+// main entry point
 
 const express = require("express");
+require("dotenv").config();
 const app = express();
+const db = require("./firebase");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/api/tweets", async (req, res) => {
+  const newTweet = {
+    user: req.body.user,
+    tweet: req.body.tweet,
+  };
+
+  const tweetRef = await db.collection("tweets").add(newTweet);
+  res.json({
+    tweetId: tweetRef.id,
+    ...newTweet,
+  });
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
